74c7a0fe244d5f2e51b23246e19e2aad17229235
roboflover <96958044+roboflover@users.noreply.github.com>
2024-05-11 02:40:49 +0300

    first commit

--- server.js
+++ server.js
@@ -4,28 +4,9 @@ const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    const data = JSON.parse(message)
-    // const data = JSON.parse(message)
-    // if(data.type === 'offer'){
-    //   sendData({
-    //     type: "offer",
-    //     offer: user.offer
-    // }, connection)
-    //console.log(data)
-
-    wss.clients.forEach(function each(client) {
-      //if(data.type === 'offer') {
-        //client.send(JSON.stringify(data));
-      //}
-    });
-  });
-});
-
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
     // Рассылка сообщения всем подключенным клиентам
     wss.clients.forEach(function each(client) {
+      console.log(client)
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         // Убедитесь, что отправляемые данные являются строкой JSON
         const messageToSend = JSON.stringify(message);
